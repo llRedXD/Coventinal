6d9bf9e74ace62b6d7d37151966d1c9a52993bbc
llRedXD <gh.o.neira@hotmail.com>
2022-07-22 14:23:56 -0300

    feat(backend): :fire: reiniciando projeto
    apaguei todos os itens do django e irei começar um novo

--- core/escola/admin.py
+++
@@ -1,8 +0,0 @@
-# Local imports
-from .models import Aluno, Curso, Matricula
-
-# Django imports
-from django.contrib import admin
-
-admin.site.register(Aluno)
-
--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from core.escola.urls import router as escola_router
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(escola_router.urls)),
-    # path('api-auth/', include(escola_router.urls), name='rest_framework'),
-]
